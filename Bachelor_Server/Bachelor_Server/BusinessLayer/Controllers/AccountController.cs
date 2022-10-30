d86ddec508804c19f0fc288471df1b4f2cbc5298
Cata011 <alex_catalin1700@yahoo.com>
2022-10-30 14:32:54 +0100

    account and more error logging

--- Bachelor_Server/Bachelor_Server/BusinessLayer/Controllers/AccountController.cs
+++ Bachelor_Server/Bachelor_Server/BusinessLayer/Controllers/AccountController.cs
@@ -1,9 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bachelor_Server.BusinessLayer.Services.Account;
+using Bachelor_Server.BusinessLayer.Services.Requests;
+using Bachelor_Server.Models.Account;
+using Bachelor_Server.Models.WorkerConfiguration;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Bachelor_Server.BusinessLayer.Controllers;
 
 [ApiController]
 public class AccountController : ControllerBase
 {
+    private IAccountService _accountService;
     
+    
+    public AccountController(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    [HttpPost]
+    public async Task<AccountModel> GetLoggedAccount()
+    {
+        var reader = new StreamReader(Request.Body);
+        reader.BaseStream.Seek(0, SeekOrigin.Begin);
+        var rawMessage = await reader.ReadToEndAsync();
+        return await _accountService.GetLoggedAccount(
+            JsonConvert.DeserializeObject<AccountModel>(rawMessage));
+    }
 }
---
+++ Bachelor_Server/Bachelor_Server/BusinessLayer/Services/Account/AccountService.cs
@@ -0,0 +1,36 @@
+﻿using Bachelor_Server.BusinessLayer.Services.Logging;
+using Bachelor_Server.BusinessLayer.Services.WorkerConfig;
+using Bachelor_Server.DataAccessLayer.Repositories.Account;
+using Bachelor_Server.Models.Account;
+
+namespace Bachelor_Server.BusinessLayer.Services.Account;
+
+public class AccountService : IAccountService
+{
+    
+    private ILogHandling _log;
+    private IAccountRepo _accountRepo;
+
+
+    public AccountService(ILogHandling log, IAccountRepo accountRepo)
+    {
+        _log = log;
+        _accountRepo = accountRepo;
+    }
+    
+    public async Task<AccountModel> GetLoggedAccount(AccountModel accountModel)
+    {
+        try
+        {
+            AccountModel account = await _accountRepo.GetAccount(accountModel);
+            await _log.Log(account.DisplayName + "logged in");
+            return account;
+        }
+        catch (Exception e)
+        {
+            await _log.LogError(e);
+        }
+
+        return new AccountModel();
+    }
+}
