529e1ebe6802487e077081cf55c457081c2488f8
Alexandru Jurj <ajurj12@yahoo.com>
2022-11-03 13:47:46 +0100

    WC repo add auth before wc

--- Bachelor_Server/Bachelor_Server/DataAccessLayer/Repositories/WorkerConfig/WorkerConfigurationRepo.cs
+++ Bachelor_Server/Bachelor_Server/DataAccessLayer/Repositories/WorkerConfig/WorkerConfigurationRepo.cs
@@ -17,6 +17,21 @@ namespace Bachelor_Server.DataAccessLayer.Repositories.WorkerConfig
         {
             await using (var context = await dbContext.CreateDbContextAsync())
             {
+                await context.Apikeys.AddAsync(workerConfigurationModel.FkApikey);
+                /*await context.BearerTokens.AddAsync(workerConfigurationModel.FkBearerToken);
+                await context.BasicAuths.AddAsync(workerConfigurationModel.FkBasicAuth);
+                await context.Oauth10s.AddAsync(workerConfigurationModel.FkOauth10);
+                await context.Oauth20s.AddAsync(workerConfigurationModel.FkOauth20);
+                await context.Raws.AddAsync(workerConfigurationModel.FkRaw);*/
+                await context.SaveChangesAsync();
+
+                /*var apiKeyID = workerConfigurationModel.FkApikey.PkApikeyId;
+                var bearerTokenID = workerConfigurationModel.FkBearerToken.PkBearerTokenId;
+                var basicAuthID = workerConfigurationModel.FkBasicAuth.PkBasicAuthId;
+                var oAuth1ID = workerConfigurationModel.FkOauth10.PkOauth10id;
+                var oAuth2ID = workerConfigurationModel.FkOauth20.PkOauth20id;
+                var rawID = workerConfigurationModel.FkRaw.PkRawId;*/
+
                 await context.WorkerConfigurations.AddAsync(workerConfigurationModel);
                 await context.SaveChangesAsync();
             }
