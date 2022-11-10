2e6bd43776db62f52b698d45644feb36b485e80f
Cata011 <alex_catalin1700@yahoo.com>
2022-11-10 14:49:02 +0100

    not working email

--- Bachelor_Server/Bachelor_Server/BusinessLayer/Services/Account/AccountService.cs
+++ Bachelor_Server/Bachelor_Server/BusinessLayer/Services/Account/AccountService.cs
@@ -1,4 +1,6 @@
-﻿using Bachelor_Server.BusinessLayer.Services.Logging;
+﻿using System.Net;
+using System.Net.Mail;
+using Bachelor_Server.BusinessLayer.Services.Logging;
 using Bachelor_Server.BusinessLayer.Services.WorkerConfig;
 using Bachelor_Server.DataAccessLayer.Repositories.Account;
 using Bachelor_Server.Models;
@@ -40,6 +42,7 @@ public class AccountService : IAccountService
         {
             await _accountRepo.CreateAccount(account);
             await _log.Log(account.DisplayName + "created");
+            SendEmail(); //TODO
         }
         catch (Exception e)
         {
@@ -51,5 +54,38 @@ public class AccountService : IAccountService
     public async Task<List<Models.Account>> GetAllUsers()
     {
         return await _accountRepo.GetAllUsers();
+    }
+
+    private void SendEmail()
+    {
+            try
+            {
+                MailMessage newMail = new MailMessage();
+                // use the Gmail SMTP Host
+                SmtpClient client = new SmtpClient("smtp.gmail.com"); 
+
+                // Follow the RFS 5321 Email Standard
+                newMail.From = new MailAddress("alex_catalin1700@yahoo.com", "caca");
+
+                newMail.To.Add("ajurj12@yahoo.com");// declare the email subject
+
+                newMail.Subject = "My First Email"; // use HTML for the email body
+
+                newMail.IsBodyHtml = true;newMail.Body = "<h1> This is my first Templated Email in C# </h1>";
+
+                // enable SSL for encryption across channels
+                client.EnableSsl = true; 
+                // Port 465 for SSL communication
+                client.Port = 465; 
+                // Provide authentication information with Gmail SMTP server to authenticate your sender account
+                client.Credentials = new System.Net.NetworkCredential("<<SENDER-EMAIL>>", "<<SENDER-GMAIL-PASSWORD>>");
+
+                client.Send(newMail); // Send the constructed mail
+                Console.WriteLine("Email Sent");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error -" +ex);
+            }
     }
 }
