611d155b9d1c1ef5dc2379929533a3fe4415a489
Alexandru Jurj <ajurj12@yahoo.com>
2022-11-24 14:44:29 +0100

    crud workers repo

---
+++ Bachelor_Server/Bachelor_Server/DataAccessLayer/Repositories/Schedule/IScheduleRepo.cs
@@ -0,0 +1,13 @@
+﻿using Bachelor_Server.Models;
+
+namespace Bachelor_Server.DataAccessLayer.Repositories.Schedule
+{
+    public interface IScheduleRepo
+    {
+        Task CreateWorker(Worker worker);
+        Task<List<Worker>> GetWorkers();
+        Task<Worker> GetWorkerById(int id);
+        Task EditWorker(Worker worker);
+        Task DeleteWorker(int id);
+    }
+}
---
+++ Bachelor_Server/Bachelor_Server/DataAccessLayer/Repositories/Schedule/ScheduleRepo.cs
@@ -0,0 +1,57 @@
+﻿using Bachelor_Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bachelor_Server.DataAccessLayer.Repositories.Schedule
+{
+    public class ScheduleRepo : IScheduleRepo
+    {
+        private IDbContextFactory<BachelorDBContext> _dbContext;
+        public ScheduleRepo(IDbContextFactory<BachelorDBContext> bachelorDBContext)
+        {
+            _dbContext = bachelorDBContext;
+        }
+        public async Task CreateWorker(Worker worker)
+        {
+            await using (var context = await _dbContext.CreateDbContextAsync())
+            {
+                await context.Workers.AddAsync(worker);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteWorker(int id)
+        {
+            await using (var context = await _dbContext.CreateDbContextAsync())
+            {
+                var delete = await context.Workers.FirstAsync(x => x.PkWorkerId == id);
+                context.Workers.Remove(delete);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task EditWorker(Worker worker)
+        {
+            await using (var context = await _dbContext.CreateDbContextAsync())
+            {
+                context.Workers.Update(worker);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Worker> GetWorkerById(int id)
+        {
+            await using (var context = await _dbContext.CreateDbContextAsync())
+            {
+                return await context.Workers.FirstAsync(x => x.PkWorkerId == id);
+            }
+        }
+
+        public async Task<List<Worker>> GetWorkers()
+        {
+            await using (var context = await _dbContext.CreateDbContextAsync())
+            {
+                return await context.Workers.ToListAsync();
+            }
+        }
+    }
+}
