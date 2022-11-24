3eae215839410cc2545e5f8d0234a8ce783da57d
Cata011 <alex_catalin1700@yahoo.com>
2022-11-24 17:30:25 +0100

    working but with error in factory

--- Bachelor_Server/Bachelor_Server/BusinessLayer/Services/ScheduleService/SingletonJobFactory.cs
+++ Bachelor_Server/Bachelor_Server/BusinessLayer/Services/ScheduleService/SingletonJobFactory.cs
@@ -16,7 +16,6 @@ public class SingletonJobFactory : IJobFactory
     public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
     {
         return _serviceProvider.GetRequiredService(bundle.JobDetail.JobType) as IJob;
-      
     }
 
     public void ReturnJob(IJob job)
