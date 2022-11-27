61d1227e93e32437ed9ffe394e72a6528d9af76f
Alexandru Jurj <ajurj12@yahoo.com>
2022-11-27 15:46:46 +0100

    schedule test

--- Bachelor_Server/ServerTest/ScheduleServiceTest.cs
+++ Bachelor_Server/ServerTest/ScheduleServiceTest.cs
@@ -1,6 +1,25 @@
-﻿namespace ServerTest;
+﻿/*using Bachelor_Server.BusinessLayer.Services.ScheduleService;
+using Bachelor_Server.Models;
+using Moq;
+using System.Security.Policy;
+
+namespace ServerTest;
 
 public class ScheduleServiceTest
 {
-    
-}
+    private Mock<IScheduleService> _scheduleServiceMock = new();
+
+    [Test]
+    public void BuildTriggerTest()
+    {
+        WorkerConfiguration modelMin = new WorkerConfiguration
+        {
+            ScheduleRate = "1/min"
+        };
+        WorkerConfiguration modelHour = new WorkerConfiguration
+        {
+            ScheduleRate = "1/h"
+        };
+
+    }
+}*/
